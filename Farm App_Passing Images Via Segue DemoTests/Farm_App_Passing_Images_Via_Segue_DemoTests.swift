c2dac301b73a03b4bfc57934b977fd8e489cce16
corwincrownover <corwincrownover@corwins-macbook-air.local>
2016-03-07 19:56:02 -0800

    Initial Commit

---
+++ Farm App_Passing Images Via Segue DemoTests/Farm_App_Passing_Images_Via_Segue_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Farm_App_Passing_Images_Via_Segue_DemoTests.swift
+//  Farm App_Passing Images Via Segue DemoTests
+//
+//  Created by Corwin Crownover on 3/7/16.
+//  Copyright © 2016 Corwin Crownover. All rights reserved.
+//
+
+import XCTest
+@testable import Farm_App_Passing_Images_Via_Segue_Demo
+
+class Farm_App_Passing_Images_Via_Segue_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
